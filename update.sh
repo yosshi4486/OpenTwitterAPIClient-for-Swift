981e2df25864f372764340173f711591afecf1e3
yosshi4486 <raiso.development@gmail.com>
2021-12-10 15:49:03 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# This is broken shell. I've used this in my local environment, so the output path doesn't locate remote path.
+# A goal of this repository is "autoupdating", that means this repository is updated everyday by GitHub action run.
+
 openapi-generator generate \
 -i https://api.twitter.com/2/openapi.json \
 -g swift5 -o /Desktop/TwitterAPI/ \
