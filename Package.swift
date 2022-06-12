99709b87c1bf8c224696503703652c382b06673b
yosshi4486 <yoshiharuseijin@gmail.com>
2022-06-12 20:03:31 +0900

    Update package name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpenAPIClient",
+    name: "OpenTwitterAPI",
     platforms: [
         .iOS(.v9),
         .macOS(.v10_11),
@@ -14,7 +14,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "OpenTwitterAPI",
-            targets: ["OpenAPIClient"]
+            targets: ["OpenTwitterAPI"]
         ),
     ],
     dependencies: [
@@ -25,7 +25,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "OpenAPIClient",
+            name: "OpenTwitterAPI",
             dependencies: ["AnyCodable", ],
             path: "Client/OpenAPIClient/Classes"
         ),
