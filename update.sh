a380dba18f7d4a95e3a22ca0ce71336759d9beca
yosshi4486 <yoshiharuseijin@gmail.com>
2022-06-13 14:44:57 +0900

    Update an "update" scripts

--- update.sh
+++ update.sh
@@ -2,5 +2,11 @@
 docker compose up
 
 # Moves package files to top level, because it is convenient for their package manager. 
-mv ./Client/Package.swift ./Package.swift
-mv ./Client/OpenTwitterClientAPI.podspec ./OpenTwitterClientAPI.podspec
+mv ./Client/OpenTwitterAPIClient.podspec ./OpenTwitterAPIClient.podspec
+
+# Update the source path
+# -i: in-place replace option
+sed -e 's/path: "OpenTwitterAPIClient\/Classes"/path: "Client\/OpenTwitterAPIClient\/Classes"/' ./Client/Package.swift > ./Package.swift
+
+# Remove unused package file
+rm ./Client/Package.swift
